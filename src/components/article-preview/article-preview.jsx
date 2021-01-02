ff11abd29b20c4917e8cfce2c198c4f614f06317
Georgy <georgynogovicin@yandex.ru>
2021-01-02 23:57:11 +0300

    api, get multiple articles

--- src/components/article-preview/article-preview.jsx
+++ src/components/article-preview/article-preview.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import classes from './article-preview.module.scss';
 
-const ArticlePreview = (props) => {
-  console.log(props);
-  return <li className={classes['preview-card']}>123</li>;
+const ArticlePreview = ({ title, body }) => {
+  return (
+    <li className={classes['preview-card']}>
+      <h3>{title}</h3>
+      <p>{body}</p>
+    </li>
+  );
+};
+
+ArticlePreview.propTypes = {
+  title: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
 };
 
 export default ArticlePreview;
